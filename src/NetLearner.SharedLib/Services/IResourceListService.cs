77a1b491ef92b5152c1b604d8c5a1ef179b3cdca
shahedc <shahedc@gmail.com>
2019-12-25 01:02:02 -0500

    WIP: updated Blazor app to complete ResourceList CRUD functionality, and updated shared service to support it

--- src/NetLearner.SharedLib/Services/IResourceListService.cs
+++ src/NetLearner.SharedLib/Services/IResourceListService.cs
@@ -9,5 +9,9 @@ namespace NetLearner.SharedLib.Services
     public interface IResourceListService
     {
         Task<List<ResourceList>> Get();
+        Task<ResourceList> Get(int id);
+        Task<ResourceList> Add(ResourceList resourceList);
+        Task<ResourceList> Update(ResourceList resourceList);
+        Task<ResourceList> Delete(int id);
     }
 }
--- src/NetLearner.SharedLib/Services/ResourceListService.cs
+++ src/NetLearner.SharedLib/Services/ResourceListService.cs
@@ -16,9 +16,39 @@ namespace NetLearner.SharedLib.Services
         {
             _context = context;
         }
+
+        public async Task<ResourceList> Add(ResourceList resourceList)
+        {
+            _context.ResourceLists.Add(resourceList);
+            await _context.SaveChangesAsync();
+            return resourceList;
+        }
+
+        public async Task<ResourceList> Delete(int id)
+        {
+            var resourceList = await _context.ResourceLists.FindAsync(id);
+            _context.ResourceLists.Remove(resourceList);
+            await _context.SaveChangesAsync();
+            return resourceList;
+        }
+
         public async Task<List<ResourceList>> Get()
         {
             return await _context.ResourceLists.ToListAsync();
+        }
+
+        public async Task<ResourceList> Get(int id)
+        {
+            var resourceList = await _context.ResourceLists.FindAsync(id);
+
+            return resourceList;
+        }
+
+        public async Task<ResourceList> Update(ResourceList resourceList)
+        {
+            _context.Entry(resourceList).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return resourceList;
         }
     }
 }
