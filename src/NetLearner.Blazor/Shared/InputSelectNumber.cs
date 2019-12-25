4e82221d7efe61eee15013fa40663dc0ceee5256
shahedc <shahedc@gmail.com>
2019-12-25 03:17:25 -0500

    WIP: Blazor form is now displaying and saving without error, BUT creating new LearningResource doesn't save the ResourceList value, only saves during Edit. Also, delete works but causes error when refreshing (after delete)

---
+++ src/NetLearner.Blazor/Shared/InputSelectNumber.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetLearner.Blazor.Shared
+{
+    public class InputSelectNumber<T> : InputSelect<T>
+    {
+        protected override bool TryParseValueFromString(string value, out T result,
+            out string validationErrorMessage)
+        {
+            if (typeof(T) == typeof(int))
+            {
+                if (int.TryParse(value, out var resultInt))
+                {
+                    result = (T)(object)resultInt;
+                    validationErrorMessage = null;
+                    return true;
+                }
+                else
+                {
+                    result = default;
+                    validationErrorMessage = "The chosen value is not a valid number.";
+                    return false;
+                }
+            }
+            else
+            {
+                return base.TryParseValueFromString(value, out result, out validationErrorMessage);
+            }
+        }
+    }
+}
