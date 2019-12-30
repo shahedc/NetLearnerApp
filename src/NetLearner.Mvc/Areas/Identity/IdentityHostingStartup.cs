d7ac8d74a83f261cbe178b781286ea45d27b031c
shahedc <shahedc@gmail.com>
2019-12-30 10:47:09 -0500

    Updated MVC + Razor Pages web apps with scaffolded Identity pages, e.g. Register, Login, etc

---
+++ src/NetLearner.Mvc/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NetLearner.SharedLib.Data;
+
+[assembly: HostingStartup(typeof(NetLearner.Mvc.Areas.Identity.IdentityHostingStartup))]
+namespace NetLearner.Mvc.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
