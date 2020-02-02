0dea3b5803b34db5cec76c5898251d5779633548
shahedc <shahedc@gmail.com>
2020-02-02 17:46:05 -0500

    added/updated classes to establish 1:1 and 1:N relationships between resources and content feed, then resources and tags, respectively.

---
+++ src/NetLearner.SharedLib/Models/ContentFeed.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace NetLearner.SharedLib.Models
+{
+    public class ContentFeed
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Feed URL")]
+        public string FeedUrl { get; set; }
+
+        public int LearningResourceId { get; set; }
+        public LearningResource LearningResource { get; set; }
+    }
+}
