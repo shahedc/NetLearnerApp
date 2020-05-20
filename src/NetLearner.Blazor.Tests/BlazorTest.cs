90fca325703c4409f25c8d0f7bb394e747fa8ac9
shahedc <shahedc@gmail.com>
2020-05-20 13:03:27 -0400

    initial Blazor Test using bUnit

---
+++ src/NetLearner.Blazor.Tests/BlazorTest.cs
@@ -0,0 +1,27 @@
+﻿using Bunit;
+using NetLearner.Blazor.Pages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NetLearner.Blazor.Tests
+{
+    public class BlazorTest : TestContext
+    {
+        [Fact]
+        public void TestCounter()
+        {
+            // Arrange
+            var counterComponent = RenderComponent<Counter>();
+            counterComponent.Find("p").MarkupMatches("<p>Current count: 0</p>");
+
+            // Act
+            var element = counterComponent.Find("button");
+            element.Click();
+
+            //Assert
+            counterComponent.Find("p").MarkupMatches("<p>Current count: 1</p>");
+        }
+    }
+}
