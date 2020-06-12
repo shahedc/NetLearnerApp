24e2f897cfdffbfbb92a1f72d2aa2d5ca3991285
shahedc <shahedc@gmail.com>
2020-06-12 16:01:13 -0400

    updated DocEngine and Worker Service to handle newer 3.1 blog posts in 2020 (WIP), and handle newlines properly again, to produce combined doc/pdf

--- experimental/DocMaker/Worker.cs
+++ experimental/DocMaker/Worker.cs
@@ -22,36 +22,36 @@ namespace DocMaker
         {
             // string array of URLs *without* any trailing slash
             string[] articleUrls = {
-                "https://wakeupandcode.com/authentication-authorization-in-asp-net-core-razor-pages",
-                "https://wakeupandcode.com/blazor-full-stack-web-dev-in-asp-net-core",
-                "https://wakeupandcode.com/cookies-and-consent-in-asp-net-core",
-                "https://wakeupandcode.com/deploying-asp-net-core-to-azure-app-service",
-                "https://wakeupandcode.com/ef-core-relationships-in-asp-net-core",
-                "https://wakeupandcode.com/forms-and-fields-in-asp-net-core",
+                "https://wakeupandcode.com/authentication-authorization-in-asp-net-core-3-1",
+                "https://wakeupandcode.com/blazor-full-stack-web-dev-in-asp-net-core-3-1",
+                "https://wakeupandcode.com/cookies-and-consent-in-asp-net-core-3-1",
+                "https://wakeupandcode.com/deploying-asp-net-core-3-1-to-azure-app-service",
+                "https://wakeupandcode.com/ef-core-relationships-in-asp-net-core-3-1",
+                "https://wakeupandcode.com/forms-and-fields-in-asp-net-core-3-1",
 
-                "https://wakeupandcode.com/generic-host-builder-in-asp-net-core",
-                "https://wakeupandcode.com/handling-errors-in-asp-net-core",
-                "https://wakeupandcode.com/iis-hosting-for-asp-net-core-web-apps",
-                "https://wakeupandcode.com/javascript-css-html-static-files-in-asp-net-core",
-                "https://wakeupandcode.com/key-vault-for-asp-net-core-web-apps",
-                "https://wakeupandcode.com/logging-in-asp-net-core",
+                "https://wakeupandcode.com/generic-host-builder-in-asp-net-core-3-1",
+                "https://wakeupandcode.com/handling-errors-in-asp-net-core-3-1",
+                "https://wakeupandcode.com/iis-hosting-for-asp-net-core-3-1-web-apps",
+                "https://wakeupandcode.com/javascript-css-html-other-static-files-in-asp-net-core-3-1",
+                "https://wakeupandcode.com/key-vault-for-asp-net-core-3-1-web-apps",
+                "https://wakeupandcode.com/logging-in-asp-net-core-3-1",
 
-                "https://wakeupandcode.com/middleware-in-asp-net-core",
-                "https://wakeupandcode.com/net-core-3-vs2019-and-csharp-8",
-                "https://wakeupandcode.com/organizational-accounts-for-asp-net-core",
-                "https://wakeupandcode.com/production-tips-for-asp-net-core-web-apps",
-                "https://wakeupandcode.com/query-tags-in-ef-core-for-asp-net-core",
-                "https://wakeupandcode.com/razor-pages-in-asp-net-core",
+                "https://wakeupandcode.com/middleware-in-asp-net-core-3-1",
+                "https://wakeupandcode.com/net-5-0-vs2019-preview-and-c-9-0-for-asp-net-core-developers",
+                "https://wakeupandcode.com/organizational-accounts-for-asp-net-core-3-1",
+                "https://wakeupandcode.com/production-tips-for-asp-net-core-3-1-web-apps",
+                "https://wakeupandcode.com/query-tags-in-ef-core-for-asp-net-core-3-1-web-apps",
+                "https://wakeupandcode.com/razor-pages-in-asp-net-core-3-1",
 
-                "https://wakeupandcode.com/summarizing-build-2019-signalr-service",
-                "https://wakeupandcode.com/tag-helper-authoring-in-asp-net-core",
-                "https://wakeupandcode.com/unit-testing-in-asp-net-core",
-                "https://wakeupandcode.com/validation-in-asp-net-core",
-                "https://wakeupandcode.com/worker-service-in-asp-net-core",
-                "https://wakeupandcode.com/xml-json-serialization-in-asp-net-core",
+                "https://wakeupandcode.com/signalr-in-asp-net-core-3-1",
+                "https://wakeupandcode.com/tag-helper-authoring-in-asp-net-core-3-1",
+                "https://wakeupandcode.com/unit-testing-in-asp-net-core-3-1",
+                "https://wakeupandcode.com/validation-in-asp-net-core-3-1",
+                "https://wakeupandcode.com/worker-service-in-net-core-3-1",
+                "https://wakeupandcode.com/xml-json-serialization-in-asp-net-core-3-1",
 
-                "https://wakeupandcode.com/yaml-defined-cicd-for-asp-net-core",
-                "https://wakeupandcode.com/zero-downtime-web-apps-for-asp-net-core"
+                "https://wakeupandcode.com/yaml-defined-cicd-for-asp-net-core-3-1",
+                "https://wakeupandcode.com/zero-downtime-web-apps-for-asp-net-core-3-1"
             };
 
 
@@ -59,7 +59,15 @@ namespace DocMaker
             for (var articleCounter = 0; articleCounter < articleUrls.Length; articleCounter++)
             {
                 _logger.LogInformation($"Making doc {articleCounter + 1} at: {DateTimeOffset.Now}");
-                DocEngine.MakeDoc(articleUrls[articleCounter]);
+                try
+                {
+                    DocEngine.MakeDoc(articleUrls[articleCounter]);
+                    _logger.LogInformation($"... URL processed.");
+                } catch (Exception ex)
+                {
+                    _logger.LogError($"... URL not processed.");
+                    _logger.LogError($"Error: {ex.Message}");
+                }
             }
             _logger.LogInformation($"Completed {articleUrls.Length} docs at: {DateTimeOffset.Now}");
 
