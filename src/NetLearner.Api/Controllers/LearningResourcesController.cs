a9047a9db021e865ed2f643b6459fa27bfc60780
shahedc <shahedc@gmail.com>
2020-06-14 12:02:35 -0400

    Added API endpoint to return sample list of learning resources

---
+++ src/NetLearner.Api/Controllers/LearningResourcesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NetLearner.SharedLib.Models;
+
+namespace NetLearner.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LearningResourcesController : ControllerBase
+    {
+        private readonly ILogger<LearningResourcesController> _logger;
+
+        public LearningResourcesController(ILogger<LearningResourcesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            // sample list of learning resources
+            return new string[] 
+            {
+                "ASP .NET Core 101", 
+                "ASP .NET Core Advanced"
+            };
+        }
+    }
+}
